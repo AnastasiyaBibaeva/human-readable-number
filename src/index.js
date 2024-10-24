c848b65c7189ddb710d7f63ad304607b230a5a48
AnastasiyaBibaeva <nasbibaeva@gmail.com>
2024-10-24 16:24:00 +0300

    update: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function toReadable (number) {
-  
+const numWords = require('num-words');
+const amountInWords = numWords(number);
+let result = amountInWords.toString().replace('and ', '');
+return result;
 }
